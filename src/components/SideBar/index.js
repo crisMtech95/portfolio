5bb674db4409baf8fc2cc0e052685de0b8f0c538
crisMtech95 <cmg.95.col@hotmail.com>
2021-09-12 13:23:01 -0400

    closing sidebar  after click

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -32,12 +32,12 @@ export default function SideBar({ setShowSideBar }) {
                 <div className="SideBar__closeImg"/>
             </div>
             <div className="SideBar__container">
-                <Link to="home" spy={true} smooth={true} duration={1000}>Home</Link >
-                <Link to="about" spy={true} smooth={true} duration={1000}>About</Link >
-                <Link to="skills" spy={true} smooth={true} duration={1000}>Skills</Link >
-                <Link to="projects" spy={true} smooth={true} duration={1000}>Projects</Link >
-                <Link to="contact" spy={true} smooth={true} duration={1000}>Contact</Link >
-                <Link to="resume" spy={true} smooth={true} duration={1000}>Resume</Link >
+                <Link to="home" spy={true} smooth={true} duration={500} onClick={() => setShowSideBar(false)}>Home</Link >
+                <Link to="about" spy={true} smooth={true} duration={500} onClick={() => setShowSideBar(false)}>About</Link >
+                <Link to="skills" spy={true} smooth={true} duration={500} onClick={() => setShowSideBar(false)}>Skills</Link >
+                <Link to="projects" spy={true} smooth={true} duration={500} onClick={() => setShowSideBar(false)}>Projects</Link >
+                <Link to="contact" spy={true} smooth={true} duration={500} onClick={() => setShowSideBar(false)}>Contact</Link >
+                <Link to="resume" spy={true} smooth={true} duration={500} onClick={() => setShowSideBar(false)}>Resume</Link >
                 <p>_________</p>
             </div>
         </div>
